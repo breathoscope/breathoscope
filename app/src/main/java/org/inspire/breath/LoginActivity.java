4ce3f78d7699175e8af0da879b5f9b1e90292454
Nicholas Blott <blottn@tcd.ie>
2019-02-11 14:49:46 +0000

    removed unused code

--- app/src/main/java/org/inspire/breath/LoginActivity.java
+++ app/src/main/java/org/inspire/breath/LoginActivity.java
@@ -1,24 +1,5 @@
 package org.inspire.breath;
-/*
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-*/
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -65,20 +46,6 @@ public class LoginActivity extends AppCompatActivity {
                 else {
                     Toast.makeText(LoginActivity.this, R.string.login_incorrect_msg, Toast.LENGTH_SHORT).show();
                 }
-//                Cipher cipher = LoginActivity.this.getSymmetricKey(mPass.getText().toString());
-//                if (cipher != null) {
-//                    byte[] bytes = "password".getBytes();
-//                    byte[] output = new byte[0];
-//                    try {
-//                        output = cipher.doFinal(bytes);
-//                        String res = new String(output);
-//                        System.out.println()
-//                    } catch (BadPaddingException e) {
-//                        e.printStackTrace();
-//                    } catch (IllegalBlockSizeException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
             }
         });
 
@@ -89,40 +56,4 @@ public class LoginActivity extends AppCompatActivity {
     private boolean doLogin() {
         return this.mUname.getText().toString().equals("uname");
     }
-
-    /*private Cipher getSymmetricKey(String input) {
-        try {
-            final byte[] salt = new byte[64];
-            SecureRandom random = new SecureRandom();
-            random.nextBytes(salt);
-
-            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-            KeySpec passwordBasedEncryptionKeySpec = new PBEKeySpec(input.toCharArray(), salt, 10000, 256);
-            SecretKey secretKeyFromPBKDF2 = secretKeyFactory.generateSecret(passwordBasedEncryptionKeySpec);
-            SecretKey key = new SecretKeySpec(secretKeyFromPBKDF2.getEncoded(), "AES");
-
-            final byte[] nonce = new byte[32];
-            random.nextBytes(nonce);
-
-            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-            GCMParameterSpec spec = new GCMParameterSpec(16 * 8, nonce);
-            cipher.init(Cipher.ENCRYPT_MODE, key, spec);
-
-            return cipher;
-
-        }
-        catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        } catch (InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }*/
 }
