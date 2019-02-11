1c37ac07402bfa263e8201abf8682105ab006a9e
Brian Whelan <whelanb7@tcd.ie>
2019-02-11 14:51:36 +0000

    Added Base Malaria Activity and Fragment

---
+++ app/src/main/java/org/inspire/breath/fragments/MalariaFragment.java
@@ -0,0 +1,36 @@
+package org.inspire.breath.fragments;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.inspire.breath.activities.*;
+
+public class MalariaFragment extends Fragment {
+
+    private MalariaViewModel mViewModel;
+
+    public static MalariaFragment newInstance() {
+        return new MalariaFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.malaria_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MalariaViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
