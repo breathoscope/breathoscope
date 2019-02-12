f5e126eb2e5b10c27e6b8dee4311595a4e626739
Eric Moloney <moloneer@tcd.ie>
2019-02-12 17:34:52 +0000

    added basic Room Entity for volunteer

---
+++ app/src/main/java/org/inspire/breath/Volunteer.java
@@ -0,0 +1,36 @@
+package org.inspire.breath;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity
+public class Volunteer {
+
+    // int is implicitly @NonNull
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int volunteerId;
+
+    private String firstName, lastName;
+
+    public Volunteer(int volunteerId, String firstName, String lastName) {
+        this.volunteerId = volunteerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getVolunteerId() {
+        return this.volunteerId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+}
