1eff5b1016cc3510e4747ddd8e844e250bfa2105
Brian Whelan <whelanb7@tcd.ie>
2019-02-13 11:05:15 +0000

    Initial photo preview

---
+++ app/src/main/java/org/inspire/breath/fragments/MalariaReviewFragment.java
@@ -0,0 +1,65 @@
+package org.inspire.breath.fragments;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.camerakit.CameraKitView;
+
+import org.inspire.breath.R;
+
+public class MalariaReviewFragment extends Fragment {
+
+    private MalariaViewModel mViewModel;
+
+    public static MalariaFragment newInstance() {
+        return new MalariaFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_malaria_review, container, false);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(getActivity()).get(MalariaViewModel.class);
+        ImageView image = getView().findViewById(R.id.imageView);
+        image.setImageBitmap(mViewModel.image);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+}
--- app/src/main/java/org/inspire/breath/fragments/MalariaViewModel.java
+++ app/src/main/java/org/inspire/breath/fragments/MalariaViewModel.java
@@ -1,7 +1,12 @@
 package org.inspire.breath.fragments;
 
 import android.arch.lifecycle.ViewModel;
+import android.graphics.Bitmap;
 
 public class MalariaViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    Bitmap image;
+
+    void SetImage(Bitmap b) {
+        image = b;
+    }
 }
