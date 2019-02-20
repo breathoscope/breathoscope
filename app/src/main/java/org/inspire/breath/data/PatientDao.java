098c453cd663da2048976a5d9d9d52f66c75e8f2
Nicholas Blott <blottn@tcd.ie>
2019-02-20 18:48:53 +0100

    fixed name issue

--- app/src/main/java/org/inspire/breath/data/PatientDao.java
+++ app/src/main/java/org/inspire/breath/data/PatientDao.java
@@ -16,7 +16,7 @@ public interface PatientDao {
     void deleteAllPatients();
     
     @Query("SELECT * FROM patients ORDER BY lastName ASC")
-    List<Patient> getAllVPatients();
+    List<Patient> getAllPatients();
 
     @Query("SELECT * FROM patients WHERE id = :id")
     List<Patient> getPatientById(int id);
