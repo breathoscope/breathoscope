2aaca2f35b3b9a46ce21368208ce7b958a78c821
Nicholas Blott <blottn@tcd.ie>
2019-02-21 22:23:47 +0100

    exposed db

--- app/src/main/java/org/inspire/breath/data/AppRoomDatabase.java
+++ app/src/main/java/org/inspire/breath/data/AppRoomDatabase.java
@@ -13,7 +13,7 @@ public abstract class AppRoomDatabase extends RoomDatabase {
 
     private static volatile AppRoomDatabase INSTANCE;
 
-    static AppRoomDatabase getDatabase(final Context context) {
+    public static AppRoomDatabase getDatabase(final Context context) {
 
         if (INSTANCE == null) {
             synchronized (AppRoomDatabase.class) {
