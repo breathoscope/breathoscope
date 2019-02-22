f3c8e9b9c1a05977ebdbdea54caac3f42bdf25df
Nicholas Blott <blottn@tcd.ie>
2019-02-23 00:06:04 +0100

    added recordings as 1 -> many relationship

--- app/src/main/java/org/inspire/breath/data/Recording.java
+++ app/src/main/java/org/inspire/breath/data/Recording.java
@@ -1,7 +1,59 @@
 package org.inspire.breath.data;
 
 
-// Encapsulates any sort of recording and the actions that can be performed on it
-public abstract class Recording {
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
 
+// Encapsulates any sort of recording and the actions that can be performed on it
+@Entity(foreignKeys = @ForeignKey(entity = Patient.class,
+                                    childColumns = "patientId",
+                                    parentColumns = "id",
+                                    onDelete = ForeignKey.CASCADE),
+        tableName = "recordings")
+
+public class Recording {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+    private int patientId;
+
+    @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
+    private byte[] blob;
+
+    private String kind;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getBlob() {
+        return blob;
+    }
+
+    public void setBlob(byte[] blob) {
+        this.blob = blob;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
 }
