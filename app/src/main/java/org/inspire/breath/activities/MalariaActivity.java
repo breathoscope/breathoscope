a1752bdefcaf1927131e70d1031dfe2edc2a711d
Brian Whelan <whelanb7@tcd.ie>
2019-03-01 13:25:37 +0000

    Minor DB stuff

--- app/src/main/java/org/inspire/breath/activities/MalariaActivity.java
+++ app/src/main/java/org/inspire/breath/activities/MalariaActivity.java
@@ -7,10 +7,11 @@ import org.inspire.breath.fragments.MalariaFragment;
 import org.inspire.breath.R;
 
 public class MalariaActivity extends AppCompatActivity {
-
+    int patientID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        patientID = savedInstanceState.getInt("Patient");
         setContentView(R.layout.malaria_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
