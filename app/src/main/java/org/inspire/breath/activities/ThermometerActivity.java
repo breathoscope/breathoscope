b31598aa03afe980cfe5d9c0fd6e87ca9da1e7dd
Brian Whelan <whelanb7@tcd.ie>
2019-03-01 10:13:41 +0000

    Integreate Temperature Recordings

---
+++ app/src/main/java/org/inspire/breath/activities/ThermometerActivity.java
@@ -0,0 +1,20 @@
+package org.inspire.breath.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.inspire.breath.fragments.ThermometerFragment;
+import org.inspire.breath.R;
+public class ThermometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thermometer_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, ThermometerFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
