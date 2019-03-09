dc40f64040ecb7653a1cc6dd5c5284e78312fc8e
Nicholas Blott <blottn@tcd.ie>
2019-03-09 14:44:51 +0000

    fixed some issues caused by the rebase

--- app/src/main/java/org/inspire/breath/activities/ResetActivity.java
+++ app/src/main/java/org/inspire/breath/activities/ResetActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.inspire.breath.LoginActivity;
 import org.inspire.breath.R;
 
 import java.security.MessageDigest;
