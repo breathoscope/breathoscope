f190e41743c8218cda90cdde427626d5ebaa2136
Ezi Ozoani <ezi@Admins-MacBook-Pro.local>
2019-03-14 15:22:20 +0000

    Malaria guide

---
+++ app/src/main/java/org/inspire/breath/activities/Refer_HC.java
@@ -0,0 +1,15 @@
+package org.inspire.breath.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.inspire.breath.R;
+
+public class Refer_HC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refer__hc);
+    }
+}
---
+++ app/src/main/java/org/inspire/breath/activities/Severe_And_Pos.java
@@ -0,0 +1,19 @@
+package org.inspire.breath.activities;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+import org.inspire.breath.R;
+
+public class Severe_And_Pos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_severe__and__pos);
+
+    }
+
+}
