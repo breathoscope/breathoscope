b7757625502075ee554401c44601bdbd0962b9b7
Ezi Ozoani <ezi@Admins-MacBook-Pro.local>
2019-03-14 15:22:20 +0000

    Malaria guide

---
+++ app/src/main/java/org/inspire/breath/activities/FeverMainQ.java
@@ -0,0 +1,32 @@
+package org.inspire.breath.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import org.inspire.breath.R;
+
+public class FeverMainQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fever_main_q);
+    }
+
+    {
+        Intent intent = new Intent(this,ChildAge.class);
+        startActivity(intent);
+
+    }
+
+
+
+    public void No_Click(View view)
+    {
+        Intent intent = new Intent(this,Understanding_results.class);
+        startActivity(intent);
+
+    }
+}
