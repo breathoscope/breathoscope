cedbac7c8d037f0081b4b0c93acb849bf1236445
Eric <moloneer@tcd.ie>
2019-03-14 14:16:46 +0000

    Story/hr input 107 (#18)
    * beginning recording frontend
    
    * more sensible view switching
    
    * added initial audio recording func, saving to device
    
    * adds Snackbar message prompting user to check microphone
    
    * changed to AudioRecord library, recording to pcm file on device working
    
    * some refactors
    
    * raw audio to wav file conversion working
    
    * fixed crash after starting second recording
    
    * refactors and some comments in HrRecordingActivity
    
    * changed pause button into functional restart recording button
    
    * restored main activity
    
    * removed done TODO

---
+++ app/src/main/java/org/inspire/breath/activities/HrRecordingActivity.java
@@ -0,0 +1,197 @@
+package org.inspire.breath.activities;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.MediaRecorder;
+import android.media.AudioRecord;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.inspire.breath.utils.RawToWavConverter;
+
+import org.inspire.breath.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class HrRecordingActivity extends AppCompatActivity {
+
+    private final int RECORDER_AUDIO_SOURCE = MediaRecorder.AudioSource.MIC;
+    private final int RECORDER_SAMPLE_RATE = 44100;
+    private final int RECORDER_CHANNEL_CFG = AudioFormat.CHANNEL_IN_MONO;
+    private final int RECORDER_AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    private final int RECORDER_BUF_SIZE = AudioRecord.getMinBufferSize(
+            RECORDER_SAMPLE_RATE,
+            RECORDER_CHANNEL_CFG,
+            RECORDER_AUDIO_FORMAT
+    );
+
+    private String rawOutputPath;
+    private String wavOutputPath;
+
+    private Thread writeThread;
+
+    private AudioRecord recorder;
+    private boolean isRecording;
+
+    private ImageButton mRecordBtn, mRestartBtn, mStopBtn;
+
+    // Requesting permission to RECORD_AUDIO
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hr_recording);
+
+        rawOutputPath = getApplicationContext().getExternalCacheDir().getAbsolutePath() + "/out.pcm";
+        wavOutputPath = getApplicationContext().getExternalCacheDir().getAbsolutePath() + "/out.wav";
+
+        isRecording = false;
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        displaySnackbar();
+
+        initViews();
+
+        mRecordBtn.setOnClickListener((v) -> {
+
+            initRecorder();
+
+            toggleVisibleButtons();
+
+            recorder.startRecording();
+            isRecording = true;
+            writeThread = new Thread(new AudioFileWriter());
+            writeThread.start();
+
+        });
+
+        mRestartBtn.setOnClickListener((v) -> {
+            stopRecording();
+            Toast.makeText(HrRecordingActivity.this, "Recording cancelled", Toast.LENGTH_SHORT).show();
+        });
+
+        mStopBtn.setOnClickListener((v) -> {
+            stopRecording();
+            rawToWav();
+        });
+
+    }
+
+    private void initViews() {
+        this.mRecordBtn = findViewById(R.id.hr_record_btn);
+        this.mRestartBtn = findViewById(R.id.hr_restart_btn);
+        this.mStopBtn = findViewById(R.id.hr_stop_btn);
+
+        mRestartBtn.setVisibility(View.GONE);
+        mStopBtn.setVisibility(View.GONE);
+    }
+
+    private void toggleVisibleButtons() {
+        if (mRecordBtn.getVisibility() == View.VISIBLE) {
+            mRecordBtn.setVisibility(View.GONE);
+            mRestartBtn.setVisibility(View.VISIBLE);
+            mStopBtn.setVisibility(View.VISIBLE);
+        } else {
+            mRecordBtn.setVisibility(View.VISIBLE);
+            mRestartBtn.setVisibility(View.GONE);
+            mStopBtn.setVisibility(View.GONE);
+        }
+    }
+
+    private void initRecorder() {
+
+        recorder = new AudioRecord.Builder()
+                .setAudioSource(RECORDER_AUDIO_SOURCE)
+                .setAudioFormat(new AudioFormat.Builder()
+                        .setEncoding(RECORDER_AUDIO_FORMAT)
+                        .setSampleRate(RECORDER_SAMPLE_RATE)
+                        .setChannelMask(RECORDER_CHANNEL_CFG)
+                        .build())
+                .setBufferSizeInBytes(RECORDER_BUF_SIZE)
+                .build();
+
+    }
+
+    private void stopRecording() {
+        toggleVisibleButtons();
+        isRecording = false;
+        recorder.stop();
+        recorder.release();
+        writeThread = null;
+    }
+
+
+    void displaySnackbar() {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.activity_hr_coordinator),
+                getString(R.string.snackbar_mic_warning),
+                Snackbar.LENGTH_INDEFINITE);
+        snackbar.show();
+    }
+
+    private void rawToWav() {
+
+        try {
+            new RawToWavConverter(RECORDER_SAMPLE_RATE).rawToWave(
+                    new File(rawOutputPath),
+                    new File(wavOutputPath)
+            );
+
+        } catch (Exception e) {
+            Toast.makeText(HrRecordingActivity.this, "WAV CONVERSION FAILED", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    // thread to save raw data to app's cache directory
+    // overwrites previous recording
+    class AudioFileWriter implements Runnable {
+        public void run() {
+
+            byte[] audioData = new byte[RECORDER_BUF_SIZE];
+
+            FileOutputStream os = null;
+            try {
+                os = new FileOutputStream(rawOutputPath);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            while (isRecording) {
+                recorder.read(audioData, 0, RECORDER_BUF_SIZE);
+                try {
+                    os.write(audioData, 0, RECORDER_BUF_SIZE );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+}
