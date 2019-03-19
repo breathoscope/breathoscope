9a4238eae257c5c43515aded8c9ecce45e33b67f
damienos96 <damiandebny@hotmail.com>
2019-03-19 12:31:28 +0000

    Fixed Danger Signs

--- app/src/main/java/org/inspire/breath/DangerSignsGuide.java
+++ app/src/main/java/org/inspire/breath/DangerSignsGuide.java
@@ -10,89 +10,90 @@ public class DangerSignsGuide extends AppCompatActivity {
     DangerSignsQuestions questions = new DangerSignsQuestions();
 
     boolean anyDangerSigns = false;
+    boolean answered = false;
+    boolean results[] = new boolean[4];
     TextView questionView;
     TextView answerView;
-
-    Button yesButton;
-    Button noButton;
+    Button leftButton;
+    Button rightButton;
     Button nextButton;
-
-    String answer;
     int questionNumber = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danger_signs_guide);
-        hideAnswer();
         questionView = (TextView)findViewById(R.id.question_txt);
         answerView = (TextView)findViewById(R.id.answer_txt);
-        yesButton = (Button)findViewById(R.id.yes_btn);
-        noButton = (Button)findViewById(R.id.no_btn);
+        leftButton = (Button)findViewById(R.id.yes_btn);
+        rightButton = (Button)findViewById(R.id.no_btn);
         nextButton = (Button)findViewById(R.id.next_btn);
 
-        updateQuestion();
+        questionView.setText(questions.retQ(questionNumber));
+        leftButton.setText(questions.retO(0));
+        rightButton.setText(questions.retO(1));
+        answerView.setVisibility(View.GONE);
+        nextButton.setVisibility(View.GONE);
 
         //Button listener 1
-        yesButton.setOnClickListener(new View.OnClickListener(){
+        leftButton.setOnClickListener(new View.OnClickListener(){
             @Override public void onClick(View view){
-                //Button logic
-
-                if (yesButton.getText() == "Yes"){
-                    answer = "Yes";
-                    displayAnswer();
+                if (questionNumber < 3 && !answered){
+                    answerView.setText(questions.retA(questionNumber));
+                    answerView.setVisibility(View.VISIBLE);
+                    nextButton.setVisibility(View.VISIBLE);
+                    anyDangerSigns = true;
+                    answered = true;
+                    results[questionNumber] = true;
                 }
-                else{
-                    answer = "";
+                else if(questionNumber == 3 && !answered){
+                    answerView.setText(questions.retA(questionNumber) + " " + questions.anyDangerSigns());
+                    answerView.setVisibility(View.VISIBLE);
+                    nextButton.setVisibility(View.GONE);
+                    answered = true;
+                    results[questionNumber] = true;
                 }
             }
         });
 
-        //Button listener 1
-        noButton.setOnClickListener(new View.OnClickListener(){
+        //Button listener 2
+        rightButton.setOnClickListener(new View.OnClickListener(){
             @Override public void onClick(View view){
-                //Button logic
-
-                if (noButton.getText() == "No"){
-                    answer = "No";
-                    questionNumber++;
-                    updateQuestion();
+                if (questionNumber < 3 && !answered){
+                    answerView.setText(questions.retA(5));
+                    answerView.setVisibility(View.VISIBLE);
+                    nextButton.setVisibility(View.VISIBLE);
+                    answered = true;
+                    results[questionNumber] = false;
                 }
-                else{
-                    answer = "";
+                else if(questionNumber == 3 && anyDangerSigns && !answered) {
+                    answerView.setText(questions.retA(questionNumber+1));
+                    answerView.setVisibility(View.VISIBLE);
+                    nextButton.setVisibility(View.GONE);
+                    answered = true;
+                    results[questionNumber] = false;
                 }
             }
         });
 
-        //Button listener 1
+        //Button listener 3
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override public void onClick(View view){
-                //Button logic
-
-                if (nextButton.getText() == "Next"){
+                if (questionNumber < 3){
                     questionNumber++;
-                    hideAnswer();
-                    updateQuestion();
+                    questionView.setText(questions.retQ(questionNumber));
+                    answerView.setVisibility(View.GONE);
+                    nextButton.setVisibility(View.GONE);
+                    answered = false;
                 }
-                else{
-                    answer = "";
+                else if(questionNumber == 3){
+                    questionNumber++;
+                    leftButton.setVisibility(View.GONE);
+                    rightButton.setVisibility(View.GONE);
+                    nextButton.setVisibility(View.GONE);
+                    answered = false;
                 }
             }
         });
-    }
-
-    private void displayAnswer(){
-        if(answer == "Yes"){
-            answerView.setText(questions.retA(questionNumber));
-            answerView.setVisibility(View.VISIBLE);
-        }
-    }
-    private void hideAnswer(){
-        answerView.setVisibility(View.GONE);
-    }
-    private void updateQuestion(){
-        questionView.setText(questions.retQ(questionNumber));
-
     }
 }
--- app/src/main/java/org/inspire/breath/DangerSignsQuestions.java
+++ app/src/main/java/org/inspire/breath/DangerSignsQuestions.java
@@ -15,12 +15,14 @@ public class DangerSignsQuestions {
             "The patient may be dehydrated, have hypoglycaemia, cerebral malaria or meningitis. Perform rapid diagnostic tests and if their result is positive, give the patient pre-referral malaria treatment." +
                     " If patient has diarrhea and shows signs of dehydration treat with oral rehydration salts solution during referral, but only if the patient is able to drink and swallow.",
             "The patient needs referral to a health center or hospital, as child might vomit all medication, oral rehydration salts solution. Can be a sign of meningitis.",
-            "This can be a sign of severe respiratory distress, general severe illness. Therapy will fail"
+            "This can be a sign of severe respiratory distress, general severe illness. Therapy will fail.",
+            "No danger signs present.",
+            "Press the 'Next' button."
     };
 
     /*A message that is to be displayed when any of the questions have been answered positively*/
-    private String anyDangersSigns() {
-        return "The patient needs urgent referral to a health center or hospital. While awaiting referral, full assessment should be performed on the patient," +
+    public String anyDangerSigns() {
+        return "\nThe patient needs urgent referral to a health center or hospital. While awaiting referral, full assessment should be performed on the patient," +
             " including if possible a malaria rapid diagnostic test, and be given rectal artesunate as a pre-referral treatment in case of severe malaria. Don't delay the referral whilst waiting for a test result." +
             " In that case, give rectal artesunate presumptively.";
     }
