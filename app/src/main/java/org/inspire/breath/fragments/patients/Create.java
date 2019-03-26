3742be5cf5bfc04ea92d72516e55ef75c2eafc2c
Nicholas Blott <blottn@tcd.ie>
2019-03-26 11:57:04 +0000

    added base files

---
+++ app/src/main/java/org/inspire/breath/fragments/patients/Create.java
@@ -0,0 +1,21 @@
+package org.inspire.breath.fragments.patients;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.inspire.breath.R;
+
+public class Create extends Fragment  {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_patient_create, container, false);
+        return root;
+    }
+}
