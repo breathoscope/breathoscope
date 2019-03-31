957bc19e60c6794792afeac70c52b956c18cf1da
Brian Whelan <whelanb7@tcd.ie>
2019-03-31 22:51:37 +0100

    Breath rate calculator

--- app/src/main/java/org/inspire/breath/data/blobs/BreathTestResult.java
+++ app/src/main/java/org/inspire/breath/data/blobs/BreathTestResult.java
@@ -2,14 +2,28 @@ package org.inspire.breath.data.blobs;
 
 import org.inspire.breath.interfaces.IBlobbable;
 
+import java.nio.ByteBuffer;
+
 public class BreathTestResult implements IBlobbable {
+
+    public int breathrate;
+
+    public void setBreathrate(int value){
+        breathrate = value;
+    }
+
+    public int getBreathrate() {
+        return breathrate;
+    }
+
     @Override
     public byte[] toBlob() {
-        return new byte[0];
+        return ByteBuffer.allocate(4).putInt(breathrate).array();
     }
 
     @Override
     public BreathTestResult consumeBlob(byte[] blob) {
-        return null;
+        breathrate = ByteBuffer.wrap(blob).getInt();
+        return this;
     }
 }
